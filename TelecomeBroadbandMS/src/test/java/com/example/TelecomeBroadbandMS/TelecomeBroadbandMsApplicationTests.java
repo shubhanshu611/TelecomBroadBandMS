6c7ae4fc501874298f6b37b12af2b863cd09156d
shubhanshu611 <106374980+shubhanshu611@users.noreply.github.com>
2023-04-04 14:15:35 +0530

    Add files via upload

---
+++ TelecomeBroadbandMS/src/test/java/com/example/TelecomeBroadbandMS/TelecomeBroadbandMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TelecomeBroadbandMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomeBroadbandMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
